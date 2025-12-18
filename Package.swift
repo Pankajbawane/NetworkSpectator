28b99892e5efbe00bf21bedc305139f770cd64f3
Pankaj Bawane <pankajbawane11@gmail.com>
2025-12-18 16:14:56 +0530

    update package file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,15 +9,12 @@ let package = Package(
         .macOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkSpectator",
             targets: ["NetworkSpectator"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NetworkSpectator"
         ),
