0cf2557ec72bce4229500be46428f7a991bf51e6
Pankaj Bawane <pankajbawane11@gmail.com>
2026-07-03 21:46:39 +0530

    implement networklogmetric to keep urlsession task metrics

---
+++ Sources/NetworkSpectator/Views/LogViews/LogMetricsView.swift
@@ -0,0 +1,21 @@
+//
+//  LogMetricsView.swift
+//  NetworkSpectator
+//
+//  Created by Pankaj Bawane on 02/07/26.
+//
+
+import SwiftUI
+
+struct LogMetricsView: View {
+    let item: LogItem
+    
+    init(item: LogItem) {
+        self.item = item
+    }
+    
+    var body: some View {
+        
+    }
+    
+}
